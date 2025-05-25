e144bb73fd88c5cd51fa45ca8ec5c3e667c70d26
ALP300 <aitorpalomino1@gmail.com>
2025-05-24 19:50:46 -0500

    Actualizar proyecto

---
+++ SEMANA 4/ejer5.js
@@ -0,0 +1,5 @@
+/*
+5. Número máximo entre cuatro números:
+Declara cuatro variables a, b, c y d con valores numéricos. Utiliza condicionales
+para determinar cuál de los cuatro números es el mayor y muéstralo.
+*/
