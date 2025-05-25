4ec48f61bf06bcd035dff26e10d1bd2510244925
Camilo90s <andresotocamilo@gmail.com>
2025-05-24 20:29:56 -0500

    Resuelve ejercicio 4

--- SEMANA 4/ejer4.js
+++ SEMANA 4/ejer4.js
@@ -2,3 +2,14 @@
 /* 4. Comparación de dos números:
 Declara dos variables a y b con valores numéricos y utiliza condicionales para
 comparar si a es mayor, menor o igual a b */ 
+
+a = parseInt(prompt("Ingrese el primer número"));
+b = parseInt(prompt("Ingrese el segundo número"));
+
+if (a > b) {
+    alert(`${a} es mayor que ${b}`);
+} else if (a < b ) {
+    alert(`${a} es menor que ${b}`);
+} else {
+    alert(`${a} = ${b}`);
+}
