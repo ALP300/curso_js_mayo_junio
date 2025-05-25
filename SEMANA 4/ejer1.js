f0b78cf77e698475f8ba874e780957179ba80062
Joel Quimis <quimisjoedl@gmail.com>
2025-05-24 21:03:49 -0500

    Ejercicio-Resuelto-1

--- SEMANA 4/ejer1.js
+++ SEMANA 4/ejer1.js
@@ -1,3 +1,21 @@
 /*
     Declara tres variables a, b y c con valores numéricos y utiliza condicionales  para determinar cuál de ellos es el mayor. 
 */
+
+
+let a = parseFloat(prompt("Ingresa el valor de 1:"));
+let b = parseFloat(prompt("Ingresa el valor de 2:"));
+let c = parseFloat(prompt("Ingresa el valor de 3:"));
+let resultado;
+
+
+if (a >= b && a >= c) {
+    resultado = a;
+} else if (b >= a && b >= c) {
+    resultado = b;
+} else {
+    resultado = c;
+}
+
+
+alert("El mayor es: " + resultado);
