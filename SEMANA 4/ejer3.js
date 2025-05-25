a77544d7057487efbf4128a2983b5a81cf75bcc7
FOX12-D <Julioechevarria200@gmail.com>
2025-05-24 19:53:02 -0500

    actualizar proyecto 3

---
+++ SEMANA 4/ejer3.js
@@ -0,0 +1,6 @@
+/*
+3. Calculadora simple:
+Declara tres variables: a (un número), operador (un operador aritmético como
+cadena), y b (otro número). Utiliza condicionales para realizar la operación
+correspondiente y muestra el resultado.
+*/
