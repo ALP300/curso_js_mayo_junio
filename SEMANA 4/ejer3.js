33c58fa208e8f1882731236fdbd1cf0a5119bd44
FOX12-D <Julioechevarria200@gmail.com>
2025-05-24 20:30:35 -0500

    actualizar proyecto

--- SEMANA 4/ejer3.js
+++ SEMANA 4/ejer3.js
@@ -4,3 +4,4 @@ Declara tres variables: a (un número), operador (un operador aritmético como
 cadena), y b (otro número). Utiliza condicionales para realizar la operación
 correspondiente y muestra el resultado.
 */
+let = 20
